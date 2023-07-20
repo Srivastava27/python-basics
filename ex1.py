390fe8aad5d6527f5a9b140df42077d7258c2584
Srivastava27 <vanshikas2623@gmail.com>
2023-07-20 15:41:46 +0530

    Strings ex

---
+++ ex1.py
@@ -0,0 +1,13 @@
+name="vanshika Srivastava"
+
+print(name.find('S'))
+print(name.find('z'))
+print(name.find('Srivastava'))
+
+print(name.replace("Srivastava" , "Khan"))
+print(name.replace("S" , "A"))
+
+print('v' in name) #v is there in the string name so it is true
+print('m' in name) #m is not there in the string so it is false
+
+print("vanshika" in name)
