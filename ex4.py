0bee975d8eee7f6d72ab61dc00baa5edbe125d45
Srivastava27 <vanshikas2623@gmail.com>
2023-08-19 12:19:08 +0530

    ex4

--- ex4.py
+++ ex4.py
@@ -1,6 +1,156 @@
+# a="V"
+# b="Vanshika"
+
+# print(a[0])
+# print(b[0])
+# print(b[1])
+# print(b[5])
+# print(b[7])
+# print(b[1:6]) #slicing
+# print(b[:6]) 
+# print(b[:]) 
+# print(b[3:]) 
+# print(b[3:8:2]) 
 
 
+# a=[1,2,3,4,True,"Vanshika"]
 
+# print(a)
+# print(type(a))
+# print(a[:])
+# print(a[3:6])
+# print(a[3:])
+# print(a[:5])
+# print(a[1:6:2])
+# print(a[-1])
+# print(a[-5:-2])
+# print(a[-4:-1])
+
+# if 3 in a:
+#     print("yes")
+# else:
+#     print("no")
+
+# if "Vanshu" in "Vanshika":
+#     print("OK")
+# else:
+#     print("NOT OK")
+
+# print(len(a))
+# if "ansh" in "Vanshika":
+#     print("yes there")
+# else:
+#     print("Not there")
+
+# if True in a:
+#     print("yup")
+# else:
+#     print("NOOO")
+
+# a=[1,2,3,4,True,"Vanshika"]
+# if "shika" in "Vanshika":
+#     print(a[2]+a[3])
+# if "arr" in "Vanshika":
+#     print("No")
+# else:
+#     print(a[0]+a[3])
+
+# a=[i for i in range(15)]
+# print(a)
+
+# a=[i*3 for i in range(15)]
+# print(a)
+
+# a=[i**2 for i in range(15)]
+# print(a)
+
+# a=[i**0.5 for i in range(15)]
+# print(a)
+
+# a=[i**0.5 for i in range(30) if i%2==0]
+# print(a)
+
+# a=[i**3 for i in range(30) if i%3!=0]
+# print(a)
+
+# a=[i**(1/3) for i in range(30) if i%3==0]
+# print(a)
+
+
+a=[1,2,3,"Vanshika",2,2]
+b=[2]
+
+# if(a==b):
+#     print("yes")
+# else:
+#     print("No")
+
+# if(b[0]==a[0:6]):
+#     print("YES")
+# else:
+#     print("NO")
+
+# for i in a:
+#     if(i==b[0]):
+#         print("Yes")
+# a=[1,2,3,"Vanshika",2,2]
+# b=[2]
+
+# for i in a:
+#     for j in b:
+#         if(i == j):
+#             print(True)
+#         else:
+#             print(False)      
+# 
+
+a=[1,2,7,8,9,5,4,7,7,2]
+
+# a.append(10)
+# print(a)
+
+# a.sort()
+# print(a)
+
+# a.sort(reverse=True)
+# print(a)
+
+# a.reverse()
+# print(a)
+
+# print(a.index(2))
+# print(a.index(7))
+
+
+# print(a.count(2))
+# print(a.count(7))
+
+# k=a.copy()
+# k[4]=10
+# print(k)
+
+# a.insert(5,50)
+# print(a)
+
+
+# m=[30,40,50]
+# a.extend(m)
+# print(a)
+
+
+# a.clear()
+# print(a)
+
+# d//.yel m
+# /print(m)
+
+
+5
+# del a[2]
+# del a[6]
+# del a[7]
+
+# print(a)
 
 
 
